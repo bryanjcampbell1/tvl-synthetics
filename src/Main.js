0ad3ce80b3e22bf247b8a9f2c5385dd997e93376
Bryan Campbell <bryanjcampbell1@gmail.com>
2020-10-23 23:31:13 -0400

    refactored Main component

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import moment from 'moment';
 import { Button,Form } from 'react-bootstrap';
-import {Line} from 'react-chartjs-2';
 
+import ProjectChart from "./ProjectChart";
 import SynthProduct from './SynthProduct';
 
 
-const axios = require('axios');
-
-
-
-
-moment().format();
-
-
-const aaveDerivatives = [
+const derivativesArray = [
     {  project:"Aave",
         tvl:'$213,244',
         expires:'11/20/20 at 5:00 PM EST',
@@ -28,206 +19,23 @@ const aaveDerivatives = [
         expires:'12/20/20 at 5:00 PM EST',
         price:'31.3244',
         opiumId:'adadtgadgadgadgat'
-
     },
-
-];
-
-const opiumDerivatives = [
     {  project:"Opium Network",
         tvl:'$74,244',
         expires:'11/20/20 at 5:00 PM EST',
         price:'74.244',
         opiumId:'adadatbsfbab'
 
-    },
+    }
+
 ];
+
 
 
 function Main(props){
     const base = props.base;
 
-    const initialData = {
-        labels: [],
-        datasets: [
-            {
-                label: 'Total Value Locked (USD) in Aave',
-                fill: false,
-                lineTension: 0.0,
-                backgroundColor: 'rgba(75,192,192,0.4)',
-                borderColor: 'rgba(75,192,192,1)',
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: 'rgba(75,192,192,1)',
-                pointBackgroundColor: '#fff',
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                pointHoverBorderColor: 'rgba(220,220,220,1)',
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: [{
-                    x: moment(0),
-                    y: 0
-                }]
-            }
-        ]
-    };
-
-    const options = {
-        scales: {
-            xAxes: [{
-                type: 'time',
-                time: {
-                    //unit: 'month'
-                    unit: 'quarter'
-                },
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true,
-                    callback: function(value, index, values) {
-                        if(parseInt(value) >= 1000000000){
-                            const billions = (value/1000000000).toFixed(2);
-                            return '$' + billions + " B";
-                        }
-                        else if(parseInt(value) >= 1000000){
-                            const millions = (value/1000000);
-                            return '$' + millions + " M";
-                        }
-                        else if(parseInt(value) >= 1000){
-                            const thousands = (value/1000);
-                            return '$' + thousands  + " K";
-                        }
-                        else {
-                            return '$' + value;
-                        }
-                    }
-                }
-            }]
-        }
-    }
-
-    const [graphData, setGraphData] = useState(initialData);
-    const [tvlUSD, setTvlUSD] = useState("");
-    const [derivativesArray, setDerivativesArray] = useState([]);
-
-/*
-
-    useEffect(() => {
-        let projectName = 'Aave';
-        loadProject(projectName);
-    });
-
-*/
-
-
-    const loadProject = async(e) => {
-        console.log(e);
-
-        let projectName = e.toString();
-
-        if(e == 'Opium Network'){
-
-            projectName = 'opium-network';
-        }
-
-        loadProjectDerivatives(projectName);
-
-        const apiKey = '**********************';
-
-
-
-        axios.get(`https://data-api.defipulse.com/api/v1/defipulse/api/GetHistory?project=${projectName}&api-key=${apiKey}`)
-            .then(function (response) {
-                // handle success
-                console.log(response.data);
-
-                let chartData = [];
-
-                for(let i =0; i < response.data.length; i++){
-
-                    let point = {
-                        x: moment(0),
-                        y: 0
-                    };
-
-                    point.x = moment(response.data[i].timestamp*1000);
-                    point.y = response.data[i].tvlUSD;
-
-                    chartData.push(point);
-                }
-
-
-
-                let gData = {
-                    labels: [],
-                    datasets: [
-                        {
-                            label: `Total Value Locked (USD) in ${e}`,
-                            fill: false,
-                            lineTension: 0.0,
-                            backgroundColor: 'rgba(75,192,192,0.4)',
-                            borderColor: 'rgba(75,192,192,1)',
-                            borderCapStyle: 'butt',
-                            borderDash: [],
-                            borderDashOffset: 0.0,
-                            borderJoinStyle: 'miter',
-                            pointBorderColor: 'rgba(75,192,192,1)',
-                            pointBackgroundColor: '#fff',
-                            pointBorderWidth: 1,
-                            pointHoverRadius: 5,
-                            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                            pointHoverBorderColor: 'rgba(220,220,220,1)',
-                            pointHoverBorderWidth: 2,
-                            pointRadius: 1,
-                            pointHitRadius: 10,
-                            data: chartData
-                        }
-                    ]
-                }
-
-                setGraphData(gData);
-
-
-                let lastValue = chartData[0].y;
-                let value = '';
-
-                if(parseInt(lastValue) >= 1000){
-                    value =  '$' + lastValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                } else {
-                    value =  '$' + lastValue;
-                }
-
-                setTvlUSD(value);
-
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-
-
-            });
-
-
-    }
-
-    const loadProjectDerivatives = async(e) => {
-
-        if(e === 'opium-network'){
-            setDerivativesArray(opiumDerivatives);
-        }
-        else{
-            setDerivativesArray(aaveDerivatives);
-        }
-    };
-
+    const [project, setProject] = useState("Aave");
 
     return(
 
@@ -240,14 +48,14 @@ function Main(props){
                 width:'60%',
                 textAlign:'center'
             }}>
-                <p> </p>
-                <Line data={graphData} options={options}  />
-                <p style={{marginTop:20, color:'grey'}}>{`Current TVL: ${tvlUSD}`}</p>
+
+                <ProjectChart project={project}  />
+
                 <Form>
                     <Form.Group  controlId="currency" style={{marginTop:15}}>
                         <Form.Label>Protocol Name</Form.Label>
                         <Form.Control as="select"
-                                      onChange={(e)=> loadProject(e.target.value)}>
+                                      onChange={(e)=> setProject(e.target.value)}>
                             <option>Aave</option>
                             <option>Opium Network</option>
                         </Form.Control>
@@ -258,7 +66,9 @@ function Main(props){
 
                 <div >
                     {
-                        derivativesArray.map((row, key) =>
+                        derivativesArray
+                            .filter(i => (i.project === project) )
+                            .map((row, key) =>
                             <SynthProduct project={row.project}
                                           tvl={row.tvl}
                                           expires={row.expires}
---
+++ src/ProjectChart.js
@@ -0,0 +1,151 @@
+import React, { useState, useEffect} from 'react';
+import './App.css';
+import moment from 'moment';
+
+import {Line} from 'react-chartjs-2';
+const axios = require('axios');
+
+moment().format();
+
+
+function ProjectChart(props){
+
+    const options = {
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    //unit: 'month'
+                    unit: 'quarter'
+                },
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    callback: function(value, index, values) {
+                        if(parseInt(value) >= 1000000000){
+                            const billions = (value/1000000000).toFixed(2);
+                            return '$' + billions + " B";
+                        }
+                        else if(parseInt(value) >= 1000000){
+                            const millions = (value/1000000);
+                            return '$' + millions + " M";
+                        }
+                        else if(parseInt(value) >= 1000){
+                            const thousands = (value/1000);
+                            return '$' + thousands  + " K";
+                        }
+                        else {
+                            return '$' + value;
+                        }
+                    }
+                }
+            }]
+        }
+    }
+
+    const [graphData, setGraphData] = useState();
+    const [tvlUSD, setTvlUSD] = useState("");
+
+
+    useEffect(() => {
+        loadProject(props.project);
+    });
+
+
+    const loadProject = async(e) => {
+
+        let projectName = e.toString();
+        if(e == 'Opium Network'){
+            projectName = 'opium-network';
+        }
+
+        const apiKey = '******************';
+        axios.get(`https://data-api.defipulse.com/api/v1/defipulse/api/GetHistory?project=${projectName}&api-key=${apiKey}`)
+            .then(function (response) {
+
+                let chartData = [];
+
+                for(let i =0; i < response.data.length; i++){
+
+                    let point = {
+                        x: moment(0),
+                        y: 0
+                    };
+
+                    point.x = moment(response.data[i].timestamp*1000);
+                    point.y = response.data[i].tvlUSD;
+
+                    chartData.push(point);
+                }
+
+
+
+                let gData = {
+                    labels: [],
+                    datasets: [
+                        {
+                            label: `Total Value Locked (USD) in ${e}`,
+                            fill: false,
+                            lineTension: 0.0,
+                            backgroundColor: 'rgba(75,192,192,0.4)',
+                            borderColor: 'rgba(75,192,192,1)',
+                            borderCapStyle: 'butt',
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: 'miter',
+                            pointBorderColor: 'rgba(75,192,192,1)',
+                            pointBackgroundColor: '#fff',
+                            pointBorderWidth: 1,
+                            pointHoverRadius: 5,
+                            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                            pointHoverBorderColor: 'rgba(220,220,220,1)',
+                            pointHoverBorderWidth: 2,
+                            pointRadius: 1,
+                            pointHitRadius: 10,
+                            data: chartData
+                        }
+                    ]
+                }
+
+                setGraphData(gData);
+
+
+                let lastValue = chartData[0].y;
+                let value = '';
+
+                if(parseInt(lastValue) >= 1000){
+                    value =  '$' + lastValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                } else {
+                    value =  '$' + lastValue;
+                }
+
+                setTvlUSD(value);
+
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+
+
+            });
+
+
+    }
+
+
+    return(
+
+        <div>
+            <Line data={graphData} options={options}  />
+            <p style={{marginTop:20, color:'grey'}}>{`Current TVL: ${tvlUSD}`}</p>
+        </div>
+    );
+}
+
+
+export default ProjectChart;
+
