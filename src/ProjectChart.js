9f6416e00190816a6e09495de09825850be59a3c
Bryan Campbell <bryanjcampbell1@gmail.com>
2020-10-24 23:19:52 -0400

    added initial state for chart

--- src/ProjectChart.js
+++ src/ProjectChart.js
@@ -44,7 +44,35 @@ function ProjectChart(props){
         }
     }
 
-    const [graphData, setGraphData] = useState();
+    const [graphData, setGraphData] = useState({
+        labels: [],
+        datasets: [
+            {
+                label: `Total Value Locked (USD)`,
+                fill: false,
+                lineTension: 0.0,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [{
+                    x: moment(0),
+                    y: 0
+                }]
+            }
+        ]
+    });
     const [tvlUSD, setTvlUSD] = useState("");
 
 
