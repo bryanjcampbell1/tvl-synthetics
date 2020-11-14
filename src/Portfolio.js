6478142f00cf845cb9a670e5b4d5bf62ce34274f
Bryan Campbell <bryanjcampbell1@gmail.com>
2020-11-14 00:35:24 -0500

    redeem at expiry links added

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,10 +1,83 @@
 import React, {useEffect, useState} from 'react';
 import {Row, Col, Form,Button} from 'react-bootstrap';
-import Main from "./Main";
 
+
+function Mint(props) {
+
+    return(
+        <div>
+            <p style={{fontSize: 18,
+                fontWeight: '600',
+                margin:20
+            }}>Mint Tokens</p>
+
+
+        </div>
+    );
+}
+
+function ManageCollateral(props) {
+
+    return(
+        <div>
+            <p style={{fontSize: 18,
+                fontWeight: '600',
+                margin:20
+            }}>Manage Collateral</p>
+        </div>
+    );
+}
+
+function RedeemEarly(props) {
+
+    return(
+        <div>
+            <p style={{fontSize: 18,
+                fontWeight: '600',
+                margin:20
+            }}>Redeem Early</p>
+        </div>
+    );
+}
+
+function RedeemAtExpiration(props) {
+
+    return(
+        <div style={{
+            display:'flex',
+            flexDirection:'column',
+            alignItems:'center'
+        }}>
+            <p style={{
+                fontSize: 18,
+                fontWeight: '600',
+                marginTop:20
+            }}>Redeem Expired</p>
+
+
+            <p style={{width:'80%'}}>The easiest way to redeem is via UMA's EMP Tools. Check out the tutorial for more info!</p>
+
+            <Button
+                style={{marginTop:20,width:'80%'}}
+                href="http://tools.umaproject.org/"
+                variant="info"
+            >Take Me To EMP Tools!</Button>
+
+            <Button
+                style={{marginTop:10,width:'80%'}}
+                href="https://docs.umaproject.org/tutorials/redeem-tokens"
+                variant="info"
+            >Tutorial</Button>
+
+
+
+        </div>
+    );
+}
 
 function Portfolio(props){
     const [project, setProject] = useState("All Projects");
+    const [action,setAction] = useState();
 
     return(
         <div>
@@ -33,16 +106,24 @@ function Portfolio(props){
                                 flexDirection:"column",
                                 alignItems:"center",
                                 width:'50%'}}>
-                                <p>My Balance</p>
-                                <p>100</p>
+                                <p style={{fontSize: 18,
+                                    fontWeight: '600',
+                                }}>My Balance</p>
+                                <p style={{fontSize: 20,
+                                    fontWeight: '900',
+                                }}>100</p>
                             </div>
                             <div style={{
                                 display:'flex',
                                 flexDirection:"column",
                                 alignItems:"center",
                                 width:'50%'}}>
-                                <p>Minted</p>
-                                <p>80</p>
+                                <p style={{fontSize: 18,
+                                    fontWeight: '600',
+                                }}>Minted</p>
+                                <p style={{fontSize: 20,
+                                    fontWeight: '900',
+                                }}>80</p>
                             </div>
                         </div>
 
@@ -52,16 +133,23 @@ function Portfolio(props){
                             alignItems:"center",
                             width:'100%',
                         }}>
-                            <Button style={button1}>MINT</Button>
-                            <Button style={button1}>MANAGE COLLATERAL</Button>
-                            <Button style={button1}>REDEEM EARLY</Button>
-                            <Button style={button1}>REDEEM EXPIRED</Button>
+                            <Button onClick={() => setAction('mint')} style={button1}>MINT</Button>
+                            <Button onClick={() => setAction('manage')} style={button1}>MANAGE COLLATERAL</Button>
+                            <Button onClick={() => setAction('redeemEarly')} style={button1}>REDEEM EARLY</Button>
+                            <Button onClick={() => setAction('redeem')} style={button1}>REDEEM EXPIRED</Button>
                         </div>
 
                     </div>
                     <div style={{
                         width:'60%'
-                    }}></div>
+                    }}>
+                        <div style={{marginLeft:40,marginTop:-20}}>
+                            {(action === 'mint')? <Mint/> : <div></div>}
+                            {(action === 'manage')? <ManageCollateral/> : <div></div>}
+                            {(action === 'redeemEarly')? <RedeemEarly/> : <div></div>}
+                            {(action === 'redeem')? <RedeemAtExpiration/> : <div></div>}
+                        </div>
+                    </div>
                 </div>
             </div>
             <div style={{height:200}}></div>
